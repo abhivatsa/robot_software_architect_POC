d1828ae8eb8b63849c1830102436a66e443ecea7
aryan-bampal <42663937+aryan-bampal@users.noreply.github.com>
2024-06-28 12:07:01 +0530

    Add files via upload

--- ethercat-code/SharedMemory.h
+++ ethercat-code/SharedMemory.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <master.h>
+#include "master.h"
 
 void EthercatMaster::configureSharedMemory()
 {
--- ethercat-code/build/CMakeFiles/ecat_master.dir/cmake_clean.cmake
+++ ethercat-code/build/CMakeFiles/ecat_master.dir/cmake_clean.cmake
@@ -1,5 +1,6 @@
 file(REMOVE_RECURSE
   "CMakeFiles/ecat_master.dir/master.cpp.o"
+  "CMakeFiles/ecat_master.dir/master.cpp.o.d"
   "ecat_master"
   "ecat_master.pdb"
 )
